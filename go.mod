c256b6442ae7093c20cbbee81f1035bcf993a770
WinterSunset95 <wintersunset95@gmail.com>
2022-11-27 16:02:22 +0530

    added whatsmeow

--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/mdp/qrterminal v1.0.1 // indirect
 	github.com/rivo/tview v0.0.0-20221117065207-09f052e6ca98 // indirect
 	github.com/rivo/uniseg v0.4.2 // indirect
 	go.mau.fi/libsignal v0.0.0-20221015105917-d970e7c3c9cf // indirect
@@ -19,4 +21,5 @@ require (
 	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	rsc.io/qr v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,34 +1,79 @@
-
-
-
-
-
-
 package main
 
 import (
 	//	"context"
 	//	"fmt"
-	//	"os"
-	//	"os/signal"
+	"os"
+	//"os/signal"
 	//	"syscall"
 	//
-	//	"go.mau.fi/whatsmeow"
-	//	"go.mau.fi/whatsmeow/store/sqlstore"
-	//	"go.mau.fi/whatsmeow/types/events"
-	//	waLog "go.mau.fi/whatsmeow/util/log"
+
+	"context"
 	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal"
+	//"github.com/mdp/qrterminal/v3"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	waLog "go.mau.fi/whatsmeow/util/log"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
+func WAConnect() (*whatsmeow.Client, error) {
+	container, err := sqlstore.New("sqlite3", "file:wapp.db?_foreign_keys=on", waLog.Noop)
+	if err != nil {
+		return nil, err
+	}
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	client := whatsmeow.NewClient(deviceStore, waLog.Noop)
+	if client.Store.ID == nil {
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			return nil, err
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+			} else {
+				fmt.Println("Login event: ", evt.Event)
+			}
+		}
+	} else {
+		err = client.Connect()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return client, nil
+}
+
 func main() {
+	cli, err := WAConnect()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var conn_status string
+	if cli.IsConnected() {
+		conn_status = "connected"
+	} else {
+		conn_status = "hehe"
+	}
+
 	array := [2]string{"Naute", "me"}
-	fmt.Println(array)
 
 	// Declaring the main application
 	app := tview.NewApplication()
+
+	message := tview.NewTextView()
+	message.SetText(conn_status)
 
 	// Root
 	box := tview.NewFlex().SetDirection(tview.FlexRow)
@@ -46,6 +91,7 @@ func main() {
 	// Users
 	left := tview.NewFlex().SetDirection(tview.FlexRow)
 	left.SetBorder(true).SetTitle("Users")
+	left.AddItem(message, 0, 1, false)
 	for i := 0; i < len(array); i++ {
 		left.AddItem(tview.NewTextView().SetText(array[i]), 0, 1, false)
 	}
@@ -61,4 +107,5 @@ func main() {
 	if err := app.SetRoot(body, true).Run(); err != nil {
 		panic(err)
 	}
+
 }
